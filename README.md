7e93470e4747c581ed2e2c0438e6d632fdabf027
Ethan Kramer <ekdevdes@gmail.com>
2012-02-07 01:00:08 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# A Prettified Localhost
+A Twitter Bootstrap 2.0 File Lister (its responsive too! Go ahead and try it out for yourself!). Inspired by [Chris Coyier](http://css-tricks.com) and [this post](http://css-tricks.com/snippets/php/display-styled-directory-contents/) specifically.
+
+##Preview
+![Preview](http://dl.dropbox.com/u/3478139/preview.png)
+
+
+##Installation
+
+To install "A Prettified Localhost", just drop the files in the root folder of your localhost directory
+
+If you have any problems you can [contact me](mailto:ethankr@comcast.net) or [create an issue](http://github.com/ekdevdes/a-prettified-localhost) here on Github.
+
+##Contribution
+
+* Fork this repo
+* Make your changes
+* Send me a pull request
+
+
+##Credits
+This script would not be possible without [Chris Coyier](http://css-tricks.com), [Andrew Champ](http://unretromedia.net/) and [this post](http://css-tricks.com/snippets/php/display-styled-directory-contents/).
---
+++ files.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
+   <link rel="stylesheet" href="bootstrap-responsive.min.css">
+   <link rel="stylesheet" href="bootstrap.min.css">
+   
+   <title>Display Directory Contents</title>
+   <style>
+     *{
+         padding:0;
+         margin:0;
+     }
+
+	#container{
+		padding:30px;
+	}
+  
+
+   </style>
+
+</head>
+<body>
+
+   <div id="container">
+       <?php
+         // opens this directory
+         $myDirectory = opendir(".");
+
+         // gets each entry
+         while($entryName = readdir($myDirectory)) {
+           $dirArray[] = $entryName;
+         }
+
+         // finds extention of file
+         function findexts ($filename)
+         {
+           $filename = strtolower($filename) ;
+           $exts = split("[/\\.]", $filename) ;
+           $n = count($exts)-1;
+           $exts = $exts[$n];
+           return strtoupper($exts);
+         }
+
+         // closes directory
+         closedir($myDirectory);
+
+         //  counts elements in array
+         $indexCount   = count($dirArray);
+
+         // sorts files
+         sort($dirArray);
+
+         // print 'em
+         print("<h1>Directory Contents</h1>");
+         print("<table width='100%' cellspacing='10' class='table'>
+                 <tr>
+                   <td class='head'>Filename</td>
+                   <td class='head'>Type</td>
+                   <td class='head'>Size</td></tr>\n");
+
+         // loops through the array of files and print them all
+         for($index=0; $index < $indexCount; $index++) {
+               if (substr("$dirArray[$index]", 0, 1) != "."){ // don't list hidden files
+					if (findexts($dirArray[$index]) == strtoupper($dirArray[$index])) {
+						echo "<tr><td><a href='$dirArray[$index]'>$dirArray[$index]</a></td>"."<td> Folder </td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
+					}else{
+						echo "<tr><td><a href='$dirArray[$index]'>$dirArray[$index]</a></td>"."<td>".findexts($dirArray[$index])."</td>"."<td>".round(filesize($dirArray[$index])/1000,2)." Mb"."</td>"."</tr>\n";
+					}
+           	   }
+         }
+         echo "</table>\n";
+       ?>
+   </div>
+
+</body>
+</html>
